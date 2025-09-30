bdad213618552eaacb53397ef531842bf6158d38
Ryan Newton <newton@meta.com>
2025-09-29 21:07:37 -0400

    [SCX] optimize first_task update logic
    This is a follow-on optimization. Before, we read the first_task using
    the same slowpath, irrespective of where we enqueue the task. With
    this change we instead base the update on what we know about the calling
    context. On both insert and removal we can break down whether the change
    definitely, never, or maybe changes first task. In some cases we know
    what the new first task will be, and can set it more directly.
    
    Signed-off-by: Ryan Newton <newton@meta.com>

--- kernel/sched/ext.c
+++ kernel/sched/ext.c
@@ -955,12 +955,15 @@ static void dispatch_enqueue(struct scx_sched *sch, struct scx_dispatch_q *dsq,
 		 */
 		rbp = rb_prev(&p->scx.dsq_priq);
 		if (rbp) {
-			struct task_struct *prev =
-				container_of(rbp, struct task_struct,
-					     scx.dsq_priq);
-			list_add(&p->scx.dsq_list.node, &prev->scx.dsq_list.node);
+			struct task_struct *prev = container_of(
+				rbp, struct task_struct, scx.dsq_priq);
+			list_add(&p->scx.dsq_list.node,
+				 &prev->scx.dsq_list.node);
+			/* First task unchanged - no update needed */
 		} else {
 			list_add(&p->scx.dsq_list.node, &dsq->list);
+			/* New task is at head - use fastpath */
+			dsq_set_first_task(dsq, p);
 		}
 	} else {
 		/* a FIFO DSQ shouldn't be using PRIQ enqueuing */
@@ -968,13 +971,19 @@ static void dispatch_enqueue(struct scx_sched *sch, struct scx_dispatch_q *dsq,
 			scx_error(sch, "DSQ ID 0x%016llx already had PRIQ-enqueued tasks",
 				  dsq->id);
 
-		if (enq_flags & (SCX_ENQ_HEAD | SCX_ENQ_PREEMPT))
+		if (enq_flags & (SCX_ENQ_HEAD | SCX_ENQ_PREEMPT)) {
 			list_add(&p->scx.dsq_list.node, &dsq->list);
-		else
+			/* New task inserted at head - use fastpath */
+			dsq_set_first_task(dsq, p);
+		} else {
+			bool was_empty;
+
+			was_empty = list_empty(&dsq->list);
 			list_add_tail(&p->scx.dsq_list.node, &dsq->list);
+			if (was_empty)
+				dsq_set_first_task(dsq, p);
+		}
 	}
-	/* Even the add_tail code path may have changed the first element. */
-	dsq_update_first_task(dsq);
 
 	/* seq records the order tasks are queued, used by BPF DSQ iterator */
 	dsq->seq++;
@@ -1028,9 +1037,14 @@ static void task_unlink_from_dsq(struct task_struct *p,
 		p->scx.dsq_flags &= ~SCX_TASK_DSQ_ON_PRIQ;
 	}
 
+	if (dsq->first_task == p) {
+		if (dsq->id & SCX_DSQ_FLAG_BUILTIN)
+			dsq_set_first_task(dsq, list_next_entry(p, scx.dsq_list.node));
+		else
+			dsq_update_first_task(dsq);
+	}
 	list_del_init(&p->scx.dsq_list.node);
 	dsq_mod_nr(dsq, -1);
-	dsq_update_first_task(dsq);
 }
 
 static void dispatch_dequeue(struct rq *rq, struct task_struct *p)
